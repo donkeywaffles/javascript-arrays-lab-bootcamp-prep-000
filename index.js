74638346a08b4940b4b0ae56c48f7f2f34e0cdf0
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2017-12-22 23:47:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const app = "I don't do much.";
 var kittens = ['Milo', 'Otis', 'Garfield'];
-var kittnes = ['Milo', 'Otis', 'Garfield'];
+
 function destructivelyAppendKitten(name) {
-  kittens = ['Milo', 'Otis', 'Garfield'];
   kittens.push(name);
   return kittens;
 }
 
 function destructivelyPrependKitten(name) {
-  kittnes = ['Milo', 'Otis', 'Garfield'];
   kittnes.unshift(name);
   return kittnes;
 }
